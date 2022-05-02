4a1635b7896542621f9801cddd08d0d435b58ce5
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-05-02 18:06:24 -0300

    Csharp: Debugger no Visual Studio

---
+++ csharp/inicio/DebuggerVisual/DebuggerVisual/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DebuggerVisual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x);
+            y = 2 * x;
+            Console.WriteLine(y);
+            z = int.Parse(Console.ReadLine());
+            Console.WriteLine(z);
+        }
+    }
+}
