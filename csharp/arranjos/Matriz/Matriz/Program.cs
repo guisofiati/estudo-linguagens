a782e46f5bf25ddf9b1b6a85647a948d2fc6ccbc
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-05-02 19:22:49 -0300

    Csharp: Vetor e matriz

---
+++ csharp/arranjos/Matriz/Matriz/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M, N;
+
+            Console.Write("Quantas linhas vai ter a matriz? ");
+            M = int.Parse(Console.ReadLine());
+            Console.Write("Quantas colunas vai ter a matriz? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("MATRIZ DIGITADA:");
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ csharp/arranjos/Vetor/Vetor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int N;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("NUMEROS DIGITADOS: ");
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1", CI));
+            }
+        }
+    }
+}
