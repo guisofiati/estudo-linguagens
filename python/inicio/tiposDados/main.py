c05acada1ea7e5a933109dbe0c07569b07ed3bb3
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-05-03 11:45:22 -0300

    Python: Primeiro programa e tipos de dados

---
+++ python/inicio/tiposDados/main.py
@@ -0,0 +1,21 @@
+# str serve para string e char
+# ponto e virgula opcional
+# python tem tipagem dinamica, nao e necessario colocar o tipo da var
+
+# idade: int
+# salario: float
+# altura: float
+# genero: str
+# nome: str
+
+idade = 20
+salario = 5800.5
+altura = 1.73
+genero = 'F'
+nome = "Maria Silva"
+
+print(f"IDADE = {idade}")
+print(f"SALARIO = {salario:.2f}")
+print(f"ALTURA = {altura:.2f}")
+print(f"GENERO = {genero}")
+print(f"NOME = {nome}")
