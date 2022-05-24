e2c6b04be221bbff91d6b740ebec47109a1620ca
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-05-24 17:01:06 -0300

    SQL: Assessment Test 1

--- sql/EXERCISES.sql
+++ sql/EXERCISES.sql
@@ -87,3 +87,33 @@ SELECT customer_id, SUM(amount) FROM payment
 WHERE staff_id = 2
 GROUP BY customer_id
 HAVING SUM(amount) > 100;
+
+/*
+
+ASSESSMENT TEST 1
+
+1) Return the customer IDs of customers who have spent at least $110
+with the staff member who has an ID of 2.
+A: 187 and 148
+
+2) How many films begin with the letter J? 
+A: 20
+
+3) What customer has the highest customer ID number whose name starts with an 'E'
+and has an address ID lower than 500?
+A: Eddie Tomlin
+
+*/
+
+SELECT customer_id, SUM(amount) FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) >= 110;
+
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'J%';
+
+SELECT first_name, last_name FROM customer
+WHERE first_name LIKE 'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1;
