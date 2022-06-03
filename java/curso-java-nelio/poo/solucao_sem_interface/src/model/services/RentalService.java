110f1016f685548b0fd4d05695fa09b28af7f174
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-06-03 14:50:40 -0300

    Java: Solucao sem interface

---
+++ java/curso-java-nelio/poo/solucao_sem_interface/src/model/services/RentalService.java
@@ -0,0 +1,61 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		// t2-t1 = diferenca em milisegundos. convertar de milisegundos para segundos = /1000 
+		// pegar a diferenca em segundos para minutos /60. diferenca de minutos para horas /60
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		}
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+
+	public Double getPricePerDay() {
+		return pricePerDay;
+	}
+
+	public void setPricePerDay(Double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+
+	public Double getPricePerHour() {
+		return pricePerHour;
+	}
+
+	public void setPricePerHour(Double pricePerHour) {
+		this.pricePerHour = pricePerHour;
+	}
+
+	public BrazilTaxService getTaxService() {
+		return taxService;
+	}
+
+	public void setTaxService(BrazilTaxService taxService) {
+		this.taxService = taxService;
+	}
+}
