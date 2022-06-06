1ef1c40df1e7883bc85f59f5404af0ccbeb5a7d9
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-06-06 15:05:59 -0300

    Java: Comparable com classe

---
+++ java/curso-java-nelio/poo/comparable_2/src/model/entities/Employee.java
@@ -0,0 +1,34 @@
+package model.entities;
+
+public class Employee implements Comparable<Employee> {
+	
+	private String name;
+	private Double salary;
+	
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	// Comparar descendente: adicionar um ' - ' 
+	@Override
+	public int compareTo(Employee otherEmployee) {
+		return -name.compareTo(otherEmployee.getName());
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+}
