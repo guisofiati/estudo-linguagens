d45d0d809b5bf6278a89f3849a539a981699c3ce
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-06-15 15:58:31 -0300

    Java: JDBC. Transacao segura de dados no banco

--- java/curso-java-nelio/poo/jdbc_1/src/application/Main.java
+++ java/curso-java-nelio/poo/jdbc_1/src/application/Main.java
@@ -1,41 +1,56 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		// deletar dados do banco
+		// transcao segura de dados do banco
 		
 		Connection conn = null;
-		PreparedStatement ps = null;
+		Statement st = null;
 		
 		try {
 			conn = DB.getConnection();
 			
-			ps = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE Id = ?"
-					);
+			// nao vai confirmar as operacoes automaticamente, so quando for requerido
+			conn.setAutoCommit(false);
 			
-			ps.setInt(1, 2);
+			st = conn.createStatement();
 			
-			int rowsAffected = ps.executeUpdate();
+			int row1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2000 WHERE DepartmentId = 1");
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+// 			Erro fake para verificar a consistencia			
+//			int x = 1;
+//			if (x < 2) {
+//				throw new SQLException("Fake error");
+//			}
+			
+			int row2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3000 WHERE DepartmentId = 2");
+			
+			// agora operacoes podem ser executadas
+			// ou executa tudo ou nada e executado
+			conn.commit();
+			
+			System.out.println("row1 = " + row1);
+			System.out.println("row2 = " + row2);
 		}
-		// integridade referencial
 		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
+			try {
+				conn.rollback();
+				throw new DbException("Transcation rolled back! Caused by: " + e.getMessage());
+			} catch (SQLException err) {
+				throw new DbException("Rollback error! Caused by: " + err.getMessage());
+			}
 		}
 		finally {
-			DB.closeStatement(ps);
+			DB.closeStatement(st);
 			DB.closeConnection();
 		}
 	}
