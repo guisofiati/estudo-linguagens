daf0b3ff7c3f9e14fe1f0dcbd0db4ee5e8559bc6
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-06-15 14:55:02 -0300

    Java: JDBC. Inserindo dados no banco

--- java/curso-java-nelio/poo/jdbc_1/src/application/Main.java
+++ java/curso-java-nelio/poo/jdbc_1/src/application/Main.java
@@ -5,8 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
 import db.DbException;
@@ -17,39 +15,21 @@ public class Main {
 		
 		// inserir dados do banco
 		
-		SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		
-		// preparar para inserir os dados
 		PreparedStatement ps = null;
 		
 		try {
 			conn = DB.getConnection();
 			
-			ps = conn.prepareStatement(
-					"INSERT INTO seller " 
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES (?, ?, ?, ?, ?)", 
-					Statement.RETURN_GENERATED_KEYS // retorna o id da nova linha criada
-					);
+			String query = "INSERT INTO department (Name) VALUES ('D1'), ('D2')";
 			
-			// substituir os place holders por seus valores
-			// 1 arg: index do placeholder, 2 arg: valor
-			ps.setString(1, "Carl purple");
-			ps.setString(2, "carl@gmail.com");
-			ps.setDate(3, new java.sql.Date(formater.parse("22/04/1985").getTime()));
-			ps.setDouble(4, 3000.0);
-			ps.setInt(5, 4);
+			ps = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 			
-			// ver quantas linhas foram atualizadas / inseridas
 			int rowsAffected = ps.executeUpdate();
 			
-			// pode ter mais de um valor, no caso foi inserido apenas 1
-			// mas se for mais imprime todos
 			if (rowsAffected > 0) {
 				ResultSet rs = ps.getGeneratedKeys();
 				while (rs.next()) {
-					// vai ser criada uma tabela auxiliar com apenas 1 coluna com os ids
 					int id = rs.getInt(1);
 					System.out.println("Done! Id = " + id);
 				}
@@ -60,9 +40,6 @@ public class Main {
 		}
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		catch (ParseException e) {
-			e.printStackTrace();
 		}
 		finally {
 			DB.closeStatement(ps);
