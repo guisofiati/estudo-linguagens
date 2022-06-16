0a43d75a5dee0c848904af7b4befcf4538ca34e5
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-06-16 09:29:44 -0300

    Java: DAO. Implementando entidades

---
+++ java/curso-java-nelio/poo/dao/src/application/Main.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department department = new Department(1, "Books");
+		System.out.println(department);
+		
+		Seller seller = new Seller(1, "Maria", "maria@gmail.com", new Date(), 3000.0, department);
+		System.out.println(seller);
+	}
+}
---
+++ java/curso-java-nelio/poo/dao/src/db/DB.java
@@ -0,0 +1,77 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	
+	// vai ler o arquivo properties criado na raiz do projeto
+	// e armazenar na variavel props
+	// vai carregar as props definidas no arquivo.
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	// conectar com banco de dados com o objeto Connection
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				// pega campo 'dburl' do arquivo properties definido na raiz
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	// fechar conexao com banco de dados
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
