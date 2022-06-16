f11f4ca69753f62924c6e288fea3bb85c3ae25d0
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-06-16 10:10:40 -0300

    Java: DAO. Implementando o find by id

--- java/curso-java-nelio/poo/dao/src/application/Main.java
+++ java/curso-java-nelio/poo/dao/src/application/Main.java
@@ -1,23 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Books");
-		System.out.println(department);
-		
-		Seller seller = new Seller(1, "Maria", "maria@gmail.com", new Date(), 3000.0, department);
-		System.out.println(seller);
-		
-		// esconde a implementacao 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 }
--- java/curso-java-nelio/poo/dao/src/model/dao/impl/SellerDaoJDBC.java
+++ java/curso-java-nelio/poo/dao/src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,27 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection conn;
+	
+	// injecao de dependencia para nao ter que criar outra
+	// conexao com o banco, criar os objetos do Connection...
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -24,8 +39,46 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			ps = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName "
+					+ "FROM seller "
+					+ "INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?"
+					);
+					
+			ps.setInt(1, id);
+			
+			rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				Department dep = new Department();
+				Seller seller = new Seller();
+				dep.setId(rs.getInt("DepartmentId"));
+				// no caso foi usado um Alias para renomear a tabela, entao deve ser passado ele
+				dep.setName(rs.getString("DepName"));
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				seller.setBirthdate(rs.getDate("BirthDate"));
+				seller.setDepartment(dep);
+				return seller;
+			}
+			// se nao tiver vendedor com o id informado retorna null
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(ps);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
